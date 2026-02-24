16bf9e5256494d77f75c39db47c351e9891ae4ba
Siddhant Rastogi <23x10sid@gmail.com>
2026-02-24 20:07:25 +0530

    LeetCode: 49. Group Anagrams (group-anagrams)

---
+++ leetcode/group-anagrams-49.-group-anagrams/solution.java
@@ -0,0 +1,16 @@
+// Problem: 49. Group Anagrams
+// URL: https://leetcode.com/problems/group-anagrams/
+// Language: java
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String , List<String>> map = new HashMap<>();
+        for(String str : strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String val = new String(arr);
+            map.putIfAbsent(val, new ArrayList<>());
+            map.get(val).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
